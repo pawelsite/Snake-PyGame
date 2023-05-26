a0e6fb03ab25d2dddbdc5a4a6f499439186fb824
“pawel.site” <pawelb303@gmail.com>
2023-05-26 21:02:14 +0200

    .

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+
+
+# TODO: stwórz klasę -> kostka węża
+# TODO: stwórz klasę -> wąż
+# TODO: stwórz funkcje rysowania siatki
+# TODO: stwórz funkcje rysowania okna
+# TODO: stwórz funkcje rysowania jabłka
+# TODO: stwórz funkcje dla okna informacyjnego
+
+def main():
+    #Main uruchamia sie na początku gry/programu i jest odpowiedzialny na jego działanie, flow etc...
+    pass
+
+
+main()
